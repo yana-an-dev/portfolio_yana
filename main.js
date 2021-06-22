a8169ee74769a8c1fd2dcb2fc5bbaf6a57683a2b
Yana <ayh916@gmail.com>
2021-06-22 13:04:26 +1000

    JS: Navbar Menu Toggle

--- main.js
+++ main.js
@@ -23,8 +23,16 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  //console.log(event.target.dataset.link);
+  navbarMenu.classList.remove("open");
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 //Handle click on "contact me" button at home
